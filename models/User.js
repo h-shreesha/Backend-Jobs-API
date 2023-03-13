ad4a91311789d1f8b46d0303da36c482344b1adc
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-13 06:15:33 +0530

    added user mod

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -12,7 +13,8 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide the email'],
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide the valid email',
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide the valid email',
         ],
         unique: true,
     },
@@ -23,9 +25,19 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-UserSchema.pre('save', async function(){
+UserSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt)
-})
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = mongoose.model('User', UserSchema)
+// UserSchema.methods.getName = function() {
+//     return this.name
+// }
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
